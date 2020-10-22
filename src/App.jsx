9abdc1c0e534c73ff2092c08f170e9ab4f0c92eb
Nelson Foltz <nelsfoltz@gmail.com>
2020-10-22 11:01:17 -0400

    add: basic structure

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import data from './data'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState([])
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log('hello world')
+  }
+
+  return (
+    <section className="section-center">
+      <h3>tired of boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          value={count}
+          onChange={e => setCount(e.target.value)}
+        />
+        <button className="btn" type="submit">
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore,
+          eum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, odio.
+        </p>
+      </article>
+    </section>
+  )
+}
+
+export default App
